b8c9039d4c40194dc333aae224ca92a4b7fdd9dd
predo maquilare <prxdrw@gmail.com>
2023-11-29 11:13:18 -0300

    ate agora o firebase nao deu problema

--- biodigestor hub/src/main.cpp
+++ biodigestor hub/src/main.cpp
@@ -6,6 +6,17 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <WiFi.h>
+#include <Firebase_ESP_Client.h>
+
+#include <addons/RTDBHelper.h>
+#include <addons/TokenHelper.h>
+
+#define WIFI_SSID "IFPB"
+#define EAP_USERNAME "20201750008" 
+#define EAP_PASSWORD "P3dr0_Gu3rr4" 
+#define DATABASE_URL "https://biodigestor-lora-default-rtdb.firebaseio.com/"
+#define API_KEY "AIzaSyAOzDmANFl73QsQ3GoFNLTFhs02e_bthd0 "
 
 #define MISO_LORA   19
 #define MOSI_LORA   27
@@ -18,10 +29,16 @@
 #define destination 0xFF
 
 U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, 15, 4, 16);
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
 
 unsigned long last_send = 0;
+unsigned long sendDataPrevMillis = 0;
 byte msgCount = 0;
 int sensors[8];
+
+bool signupOK = false;
 
 String incoming;
 String mensagem;
@@ -30,6 +47,8 @@ char incomingcopy[150] = {"teste"};
 void onReceive(int packetSize);
 void sendMessage(String outgoing);
 void StringToInt(int *i, char *s);
+void upFirebase();
+void initialconection();
 
 void setup() {
   Serial.begin(115200);
@@ -41,7 +60,7 @@ void setup() {
   {             
     while (1);                      
   }
-  
+  initialconection();
 }
 
 //strlen(sensors[i])/2
@@ -115,4 +134,49 @@ void StringToInt(int *i, char *s) {
     token = strtok(NULL, " ");
     o++;
   }
+}
+
+void upFirebase() {
+  unsigned long prev = 0;
+  byte i = 0;
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis >= 5000))  {
+    sendDataPrevMillis = millis();
+      while(i < 4) {
+        if(millis() - prev >= 500) {
+          prev = millis();
+          Firebase.RTDB.set(&fbdo, ("/biodigestor/Sensor" + String(i+1)) , sensors[i]);
+          i++;
+        }
+      }
+  }
+}
+
+void initialconection() {
+  WiFi.begin(WIFI_SSID, WPA2_AUTH_PEAP, EAP_USERNAME, EAP_USERNAME, EAP_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(10);
+  } 
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+  Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+  config.token_status_callback = tokenStatusCallback;
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+  config.tcp_data_sending_retry = 1;
+  config.timeout.socketConnection = 5000;
+  if (Firebase.signUp(&config, &auth, "", "")) {
+    Serial.println("Ok");
+    signupOK = true;
+  }
+  else {
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+  fbdo.setBSSLBufferSize(16384, 16384);
+  Firebase.reconnectNetwork(true);
+  Firebase.begin(&config, &auth);
+  Firebase.setDoubleDigits(5);
 }
